407672e4a95860aa2c1c11c40bdfeb0a7c5d6945
K0pyPaste <92143266+K0pyPaste@users.noreply.github.com>
2022-05-30 12:17:08 -0300

    Initial commit

---
+++ Simple-Login-System/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Login_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nome: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Senha: ");
+            string senha = Console.ReadLine();
+
+            if (File.Exists("login.txt"))
+            {
+                if (File.ReadAllText("login.txt").Contains(username + ":" + senha))
+                {
+                    Console.WriteLine("Sucesso ao logar");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Falha ao logar");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                File.WriteAllText("login.txt", username + ":" + senha);
+                Console.WriteLine("Login criado com sucesso");
+                Console.ReadLine();
+            }
+        }
+    }
+}
